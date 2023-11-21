48e1ba1d560e67879d1c2b3bcec8fb02428f9b11
Andrew Hudson <ahudson@scalable.com>
2023-11-21 16:26:25 +0000

    add framer-motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import "./App.css";
 
+import { motion } from "framer-motion";
+
 function App() {
   const [selected, setSelected] = useState<number | null>(null);
 
@@ -31,15 +33,34 @@ function App() {
 
       {/* If there is something selected, we show the drawer */}
       {selected !== null && (
-        <div className="drawer">
-          Drawer - {selected + 1}{" "}
-          <span className="close" onClick={handleClose}>
-            x
-          </span>
-        </div>
+        <Drawer selected={selected} handleClose={handleClose} />
       )}
     </>
   );
 }
 
 export default App;
+
+const Drawer = ({
+  selected,
+  handleClose,
+}: {
+  selected: number;
+  handleClose: () => void;
+}) => {
+  return (
+    <motion.div
+      animate={{ y: 100 }}
+      transition={{ type: "spring", stiffness: 100 }}
+      initial="hidden"
+      // animate="show"
+      className="drawer"
+      key="selected"
+    >
+      Drawer - {selected + 1}{" "}
+      <span className="close" onClick={handleClose}>
+        x
+      </span>
+    </motion.div>
+  );
+};
